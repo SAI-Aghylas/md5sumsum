6689e74c1c355b748953bfdbb8154684316dd155
aghylassai <aghilassai@gmail.com>
2020-05-31 18:20:36 +0200

    first comm

--- src/main.rs
+++ src/main.rs
@@ -2,23 +2,20 @@ extern crate walkdir;
 use std::{env, process::Command};
 
 fn main() {
-    let mut paths: Vec<String> = vec![];
+    let mut hashcat = String::from("");
     for arg in env::args().skip(1) {
         for entry in walkdir::WalkDir::new(arg)
             .into_iter()
             .filter_map(Result::ok)
             .filter(|e| !e.file_type().is_dir())
         {
-            paths.push(String::from(entry.path().to_string_lossy()))
+            let mut p =entry.path().to_string_lossy();
+            let mut cmd = Command::new("sh");
+            cmd.arg("-c").arg(format!("md5sum \"{}\"", p));
+            let mut output = String::from_utf8_lossy(&cmd.output().unwrap().stdout).to_string();
+            let mut vec: Vec<&str> = output.split(" ").collect();
+            hashcat += vec[0];
         }
-    }
-    let mut hashcat = String::from("");
-    for f in paths {
-        let mut cmd = Command::new("sh");
-        cmd.arg("-c").arg(format!("md5sum \"{}\"", f));
-        let output = String::from_utf8_lossy(&cmd.output().unwrap().stdout).to_string();
-        let vec: Vec<&str> = output.split(" ").collect();
-        hashcat += vec[0];
     }
     let mut cmd = Command::new("sh");
     cmd.arg("-c").arg(format!("echo \"{}\" | md5sum", hashcat));
